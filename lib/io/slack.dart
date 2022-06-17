cd46e6ee59473fb5b8839a7de563d98b460382a1
jeremie <demol@oxania.io>
2022-06-17 10:25:39 +0200

    update dependencies to http: ^0.13.4

--- lib/io/slack.dart
+++ lib/io/slack.dart
@@ -1,9 +1,9 @@
 library slack_io;
 
-import 'dart:async';
-import 'dart:io';
 import 'package:http/http.dart' as http;
+
 import '../src/slacksrc.dart';
+
 export '../src/slacksrc.dart';
 
 class Slack {
@@ -14,6 +14,6 @@ class Slack {
   /// The messages will go to whatever channel the token was set up for.
   send(Message m) {
     String payload = m.toString();
-    http.post(url, body: {'payload': payload});
+    http.post(Uri.parse(url), body: {'payload': payload});
   }
 }
